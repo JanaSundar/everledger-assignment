7cf1c5f047fce68a9a5bb0607ddcfda6c68ee81b
Jana <janasundar1997@gmail.com>
2021-02-16 17:55:00 +0530

    All Features Added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@/utils/index';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS {...{ theme }}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
